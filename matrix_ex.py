3a85d926b340bf926ab03b01b9d11c2166fb77ab
Grant <gjamespowell@gmail.com>
2017-04-30 17:45:25 -0400

    changed the templates

--- matrix_ex.py
+++ matrix_ex.py
@@ -1,10 +1,9 @@
 from flask import Flask, render_template, request, make_response
 from model import schedule_optimize
 from helper_functions import make_weights_from_input_dict
+from helper_functions import writejson, loadjson
 
 app = Flask(__name__)
-
-
 
 @app.route('/')
 def home():
